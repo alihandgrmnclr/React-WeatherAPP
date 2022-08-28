85abaec4e6a53039ebd555c290219e14c1849d7b
Alihan Degirmenciler <alihandegirmenciler@gmail.com>
2022-08-28 17:36:17 +0300

    map function added

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -11,22 +11,28 @@ function App() {
   const [city, setCity] = useState("");
   const [loading, setLoading] = useState(true);
 
+  const Swal = require('sweetalert2') //sweetalert2
+
   const getWeather = () => {
     axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&lang=en&days=7&key=${apiKey2}`)
       .then((response) => {
         setWeather(response)
       })
-      .catch((error) => console.log(error))
+      .catch(() => Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Something went wrong!',
+        footer: 'Make sure city name is correct'
+      }))
       .finally(setLoading(false))
   }
+
   const getDay = (day) => {
     const objToday = new Date();
     const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const dayOfWeek = weekday[objToday.getDay() + day];
     return dayOfWeek;
   }
-
-
 
   return (
     <div className="app-body">
@@ -48,57 +54,26 @@ function App() {
             </div>
             :
             <div className="container mt-3">
-              {loading && <div>Loading...</div>}
-              {!loading &&
-                <div className="row">
-                  <div className="col-sm-12">
-                    <h3>{weather.data.city_name}, {weather.data.country_code}</h3>
-                  </div>
-                  <div className="col-sm-12">
-                    <div className="row">
-                      <div className="col-sm-3">
-                        {weather.data.data[0].valid_date}<br />
-                        {getDay(0)} <br />
-                        Average<b> {weather.data.data[0].temp}</b> °C <br />
-                        Max<b> {weather.data.data[0].max_temp}</b> °C <br />
-                        Min<b> {weather.data.data[0].min_temp}</b> °C <br />
-                        {weather.data.data[0].weather.description}  <br />
-                        <img src={`https://www.weatherbit.io/static/img/icons/${weather.data.data[0].weather.icon}.png`} alt="" />
+              <div className="row">
+                <div className="col-sm-12">
+                  <h3>{weather.data.city_name}, {weather.data.country_code}</h3>
+                </div>
+                <div className="col-sm-12">
+                  <div className="row">
+                    {weather?.data?.data.map((value, index) => (
+                      <div className="col-sm-3 mb-4" key={index}>
+                        <div >{value.datetime}</div>
+                        {getDay(index)}<br />
+                        Average<b> {value.temp}</b> °C <br />
+                        Max<b> {value.max_temp}</b> °C <br />
+                        Min<b> {value.min_temp}</b> °C <br />
+                        {value.weather.description}  <br />
+                        <img src={`https://www.weatherbit.io/static/img/icons/${value.weather.icon}.png`} alt="" />
                       </div>
-                      <div className="col-sm-3">
-                        {weather.data.data[1].valid_date} <br />
-                        {getDay(1)} <br />
-                        Average<b> {weather.data.data[1].temp}</b> °C <br />
-                        Max<b> {weather.data.data[1].max_temp}</b> °C <br />
-                        Min<b> {weather.data.data[1].min_temp}</b> °C <br />
-                        {weather.data.data[1].weather.description}  <br />
-                        <img src={`https://www.weatherbit.io/static/img/icons/${weather.data.data[1].weather.icon}.png`} alt="" />
-
-                      </div>
-                      <div className="col-sm-3">
-                        {weather.data.data[2].valid_date} <br />
-                        {getDay(2)} <br />
-                        Average<b> {weather.data.data[2].temp}</b> °C <br />
-                        Max<b> {weather.data.data[2].max_temp}</b> °C <br />
-                        Min<b> {weather.data.data[2].min_temp}</b> °C <br />
-                        {weather.data.data[2].weather.description}  <br />
-                        <img src={`https://www.weatherbit.io/static/img/icons/${weather.data.data[2].weather.icon}.png`} alt="" />
-
-                      </div>
-                      <div className="col-sm-3">
-                        {weather.data.data[3].valid_date} <br />
-                        {getDay(3)} <br />
-                        Average<b> {weather.data.data[3].temp}</b> °C <br />
-                        Max<b> {weather.data.data[3].max_temp}</b> °C <br />
-                        Min<b> {weather.data.data[3].min_temp}</b> °C <br />
-                        {weather.data.data[3].weather.description}  <br />
-                        <img src={`https://www.weatherbit.io/static/img/icons/${weather.data.data[3].weather.icon}.png`} alt="" />
-
-                      </div>
-                    </div>
+                    ))}
                   </div>
                 </div>
-              }
+              </div>
             </div>
         }
       </div>
