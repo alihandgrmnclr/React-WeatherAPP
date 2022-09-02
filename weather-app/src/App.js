d0a4f364c49a534935d2bcc70507388108d18f8c
Alihan Degirmenciler <alihandegirmenciler@gmail.com>
2022-09-02 22:19:27 +0300

    day fix

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -2,6 +2,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css"
 import axios from "axios"
 import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, NavLink } from "react-router-dom";
+import Favourites from './components/Favourites';
+import Home from './components/Home';
+import Error from './components/Error';
 
 function App() {
 
@@ -28,55 +32,64 @@ function App() {
   }
 
   const getDay = (day) => {
-    const objToday = new Date();
-    const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const dayOfWeek = weekday[objToday.getDay() + day];
+    const objToday = new Date(day);
+    const Day = objToday.getDay();
+    const weekDay = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const dayOfWeek = weekDay[Day];
     return dayOfWeek;
   }
 
   return (
     <div className="app-body">
-
-      <div id='myContainer' className='text-center'>
-        <h1>Welcome to My Weather App</h1> <br />
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-12 d-flex justify-content-center">
-              <input className="form-control mb-2" type="text" placeholder='City Name' value={city} onChange={(e) => setCity(e.target.value)} />
+      <Router>
+        <div id='myContainer' className='text-center'>
+          <h1>Welcome to My Weather App</h1> <br />
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-12 d-flex justify-content-center">
+                <input className="form-control mb-2" type="text" placeholder='City Name' value={city} onChange={(e) => setCity(e.target.value)} />
+              </div>
             </div>
           </div>
-        </div>
-        <button className='btn btn-primary' onClick={getWeather}>Search</button>
-        {
-          weather.length < 2 ?
-            <div className="mt-2">
-              Please enter city name
-            </div>
-            :
-            <div className="container mt-3">
-              <div className="row">
-                <div className="col-sm-12">
-                  <h3>{weather.data.city_name}, {weather.data.country_code}</h3>
-                </div>
-                <div className="col-sm-12">
-                  <div className="row justify-content-center">
-                    {weather?.data?.data.map((value, index) => (
-                      <div className="col-sm-3 mb-4 " key={index}>
-                        <div >{value.datetime}</div>
-                        {getDay(index)}<br />
-                        Average<b> {value.temp}</b> °C <br />
-                        Max<b> {value.max_temp}</b> °C <br />
-                        Min<b> {value.min_temp}</b> °C <br />
-                        {value.weather.description}  <br />
-                        <img src={`https://www.weatherbit.io/static/img/icons/${value.weather.icon}.png`} alt="" />
-                      </div>
-                    ))}
+          <button className='btn btn-primary' onClick={getWeather}>Search</button>
+          {
+            weather.length < 2 ?
+              <div className="mt-2">
+                Please enter city name <br />
+                <NavLink to="favourites">Your Favourites</NavLink> 
+              </div>
+              :
+              <div className="container mt-3">
+                <div className="row">
+                  <div className="col-sm-12">
+                    <h3>{weather.data.city_name}, {weather.data.country_code}</h3>
+                  </div>
+                  <div className="col-sm-12">
+                    <div className="row justify-content-center">
+                      {weather?.data?.data.map((value, index) => (
+                        <div className="col-sm-3 mb-4 " key={index}>
+                          <div >{value.datetime}</div>
+                          {getDay(value.datetime)}<br />
+                          Average<b> {value.temp}</b> °C <br />
+                          Max<b> {value.max_temp}</b> °C <br />
+                          Min<b> {value.min_temp}</b> °C <br />
+                          {value.weather.description}  <br />
+                          <img src={`https://www.weatherbit.io/static/img/icons/${value.weather.icon}.png`} alt="" />
+                        </div>
+                      ))}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-        }
-      </div>
+          }
+        </div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='favourites' element={<Favourites/>}/>
+          <Route path='*' element={<Error/>}/>
+        </Routes>
+      </Router>
+
     </div>
   );
 }
---
+++ weather-app/src/components/Error.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Error() {
+  return (
+    <div>
+      <h1>
+        404 PAGE NOT FOUND!
+      </h1>
+      <button>Go To HomePage</button>
+    </div>
+  )
+}
+
+export default Error
